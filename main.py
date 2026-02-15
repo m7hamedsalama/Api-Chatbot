dfe695c7761e82900fbf9ee9003cb1cc0470f4e2
mohamed salama <159030624+m7hamedsalama@users.noreply.github.com>
2026-02-15 15:39:03 +0200

    main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 # ==============================================================================
 
 # 1. إعدادات المشروع
-MY_API_KEY = os.environ.get("GEMINI_API_KEY")
+MY_API_KEY = os.environ.get("GOOGLE_API_KEY")
 
 MODEL_NAME = "gemini-2.5-flash"
 
